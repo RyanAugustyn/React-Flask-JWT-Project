90e30b8191c08951c898ed73b616921117cddb7e
Chris Pierson <cpierswim@gmail.com>
2023-04-14 15:15:02 -0400

    NEW: Added UserReviewsResource, get and post

--- backend/resources/books.py
+++ backend/resources/books.py
@@ -1,21 +1,23 @@
 from flask import request
 from flask_jwt_extended import jwt_required, get_jwt_identity, verify_jwt_in_request
 from flask_restful import Resource
-from database.models import db, Car
-from database.schemas import car_schema, cars_schema
+from database.models import db
+from database.schemas import favorties_schema, favorite_schema
 from database.models import Favorite
 
-class AllReviewsResource(Resource):
+class UserReviewsResource(Resource):
+    @jwt_required()
     def get(self):
-        reviews = Favorite.query.all()
-        
+        user_id = get_jwt_identity()
+        user_reviews = Favorite.query.filter_by(user_id=user_id)
+        return favorties_schema.dump(user_reviews), 200
 
-
-
-
-    def get(self):
-        cars = Car.query.all()
-        make = request.args.get('make')
-        if make:
-            cars = Car.query.filter_by(make=make)
-        return cars_schema.dump(cars), 200
+    @jwt_required()
+    def post(self):
+        userid = get_jwt_identity()
+        form_data = request.get_json()
+        new_favorite = favorite_schema.load(form_data)
+        new_favorite.user_id = userid
+        db.session.add(new_favorite)
+        db.session.commit()
+        return favorite_schema.dump(new_favorite), 201
